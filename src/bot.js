00478c7dfb6ee5d124cfec157380a5b4ff847bb7
jeffreygoldsmith <jgoldsmith@rsgc.on.ca>
2016-12-03 02:01:19 -0500

    Reworked the way that functionality was implemented to make it cleaner, improved formatting consistency, and added components folder for future use.

--- src/bot.js
+++ src/bot.js
@@ -4,34 +4,30 @@
  * Description: A discord bot with arbitrary functions
  */
 
-const TOKEN = 'MjQzODk3NDI2ODYyMTQ1NTM2.Cv1yJw.9m-SXSeZj8d-OwrgDYOt3aZggVA'; // Bot token
-
-// Import dependencies
+const jekbot = {token: 'MjU0NDQ1NjI0MjAyNzU2MDk2.CyPKew.3JEyRFwd-Ym3QdRK90FZ0I-L7aA', id: '254445624202756096'};
 const Discord = require('discord.js'); // Import the discord.js module
-const ytdl = require('ytdl-core');
-const wavFileInfo = require('wav-file-info'); // Import .wav metadata grabber API
+const MessageHandler = require('../lib/components/MessageHandlers.js');
 const bot = new Discord.Client(); // Create an instance of a Discord Client, and call it bot
 
-const slowpokeSoundNames = [
-  'slowpoke.wav',
-  'slowpokelong.wav',
-  'slowpokeslow.wav',
-  'slowslowslow.wav',
-];
-
-const RANDOM_MAX = slowpokeSoundNames.length - 1;
-const RANDOM_MIN = 0;
-const CARLOS_ID = '173595969311604736';
-const DEFAULT_DELAY = 10;
+var messageQueue = [];
 
 
-// Return a random number within a range.
-const getRandomInt = (min, max) =>
+//
+// Check if a channel was found if voice communication is required
+//
+const channelFound = (message, flg) =>
 {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  // If the user is not in a voice channel, reply with an error message
+  if (!flg)
+  {
+    message.reply('Silly Billy, you have to be in a voice channel to do that!');
+  }
 }
 
 
+//
+// Event listener for bot startup.
+//
 bot.on('ready', () => {
  console.log('I am ready!'); // Signal that bot has launched
 });
@@ -44,99 +40,31 @@ bot.on('message', message => {
   var isChannelFound = false; // Flag to detect if a voice channel corresponding to the message author is found
   const author = message.author; // Message author
 
-
   // Check message content
   if (message.content === '!slow')
   {
-    // Search for the voice channel that the user is in
-    message.guild.channels.forEach((channel) => {
-      if (channel.type === 'voice')
-      {
-        channel.members.forEach((member) => {
-          if (member.id === author.id)
-          {
-            isChannelFound = true;
-            const randomNum = getRandomInt(RANDOM_MIN, RANDOM_MAX); // Get random number to index audio file array
-            const path = '../lib/Sounds/' + slowpokeSoundNames[randomNum]; // Choose audio file to play and create a path string for it
-
-            // Enter the channel that the user is currently in
-            channel.join().then(connection => {
-              const dispatcher = connection.playFile(path); // Play the file
-
-              // Get the length of the audio from the file metadata
-              wavFileInfo.infoByFilename(path, function (err, info) {
-                if (err) throw err; // Exit if an error is non nil
-
-                // Disconnect after the duration of the audio clip in miliseconds
-                setTimeout(() => {
-                  connection.disconnect();
-                }, info.duration * 1000);
-              });
-            }).catch(console.error); // Print any errors
-          }
-        });
-      }
-    });
+    isChannelFound = MessageHandler.slow(message, author);
 
     // If the user is not in a voice channel, reply with an error message
-    if (!isChannelFound)
-    {
-      message.reply('Silly Billy, you have to be in a voice channel to do that!');
-    }
+    channelFound(message, isChannelFound);
   }
 
 
   // Check message content
   if (message.content.startsWith('!stream'))
   {
-    const parsedInput = message.content.split(' ');
-
-    // Search for the voice channel that the user is in
-    message.guild.channels.forEach((channel) => {
-      if (channel.type === 'voice')
-      {
-        channel.members.forEach((member) => {
-          if (member.id === author.id)
-          {
-            isChannelFound = true;
-
-            // play streams using ytdl-core
-            const streamOptions = { seek: 0, volume: 0.1 };
-            channel.join().then(connection => {
-              const stream = ytdl(parsedInput[1], {filter : 'audioonly'});
-              const dispatcher = connection.playStream(stream, streamOptions);
-            })
-            .catch(console.error);
-          }
-        });
-      }
-    });
+    isChannelFound = MessageHandler.stream(message, author);
 
     // If the user is not in a voice channel, reply with an error message
-    if (!isChannelFound)
-    {
-      message.reply('Silly Billy, you have to be in a voice channel to do that!');
-    }
+    channelFound(message, isChannelFound);
   }
 
 
   // Check message content
   if (message.content === '!stop')
-  {
-    // Search for the voice channel that the user is in
-    message.guild.channels.forEach((channel) => {
-      if (channel.type === 'voice')
-      {
-        channel.members.forEach((member) => {
-          if (member.id === author.id)
-          {
-            channel.leave();
-          }
-        });
-      }
-    });
-  }
+    isChannelFound = MessageHandler.leave(message, jekbot);
 });
 
+
 // Log Jekbot in
-bot.login(TOKEN);
+bot.login(jekbot.token);
